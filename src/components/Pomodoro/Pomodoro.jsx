78315ba8e68cf88b490be7b7edf462b6dff39cb8
HuguesBriqueler <hugues.briqueler@gmail.com>
2022-03-31 20:52:03 +0200

    Added session timer

--- src/components/Pomodoro/Pomodoro.jsx
+++ src/components/Pomodoro/Pomodoro.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useReducer } from "react";
 import styles from "./Pomodoro.module.css";
 import pauseIcon from "../../Images/pause.svg";
 import playIcon from "../../Images/play.svg";
@@ -6,10 +6,40 @@ import resestIcon from "../../Images/reset.svg";
 
 function Pomodoro() {
   const [sessionDuration, setSessionDuration] = useState(1500);
-  const [sessionTimer, setSessionTimer] = useState(1500);
+  const [sessionTimer, setSessionTimer] = useState(10);
   const [breakDuration, setBreakDuration] = useState(300);
   const [breakTimer, setBreakTimer] = useState(300);
   const [isRunning, setIsRunning] = useState(false);
+
+  const playPause = () => {
+    setIsRunning(!isRunning);
+  };
+  const [state, dispatch] = useReducer(reducer);
+  function reducer(state, action) {
+    switch (action.type) {
+      case "TICK":
+        if (sessionTimer >= 0) {
+          setSessionTimer((prevSessionTimer) => prevSessionTimer - 1);
+        } else if (breakTimer >= 1) {
+          setBreakTimer((prevBreakTimer) => prevBreakTimer - 1);
+        } else {
+          setSessionTimer(sessionDuration);
+          setBreakTimer(breakDuration);
+        }
+        break;
+      default: {
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        dispatch({ type: "TICK" });
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [isRunning]);
 
   return (
     <div className={styles.containerChrono}>
@@ -29,8 +59,12 @@ function Pomodoro() {
         <span>CHRONO</span>
       </h1>
       <div className={styles.containerControllers}>
-        <img src={isRunning ? pauseIcon : playIcon} alt="Play/Pause button" />
-        <img src={resestIcon} alt="Reset button" />
+        <button onClick={playPause}>
+          <img src={isRunning ? pauseIcon : playIcon} alt="Play/Pause button" />
+        </button>
+        <button>
+          <img src={resestIcon} alt="Reset button" />
+        </button>
       </div>
     </div>
   );
